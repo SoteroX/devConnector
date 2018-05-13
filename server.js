2b0c1833b712df36b7e110b44bff9390c203cf9a
SoteroX <soterox13@gmail.com>
2018-05-12 20:30:16 -0500

    Initial Upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+//DB CONFIG
+const db = require('./config/keys').mongoURI;
+//Connect to MongoDB
+mongoose.connect(db)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log("ERROR: Couldn't connect to MongoDB: ", err));
+
+//Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+app.get('/',(req, res) => {
+    res.send("HELLO");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Server is running');
+});

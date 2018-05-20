33aaa42345c089b2b1ae80d6900afb1f4bcf9a5b
SoteroX <soterox13@gmail.com>
2018-05-20 18:26:30 -0500

    Logout/Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import {setCurrentUser, logoutUser} from './actions/authActions';
 //Style
 import './App.css';
 //Components
@@ -10,6 +13,24 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+//Check for token
+if(localStorage.jwtToken){
+  setAuthToken(localStorage.jwtToken);
+  //Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set user and is Authenticated
+  store.dispatch(setCurrentUser(decoded));
+  //Check for expire token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime ){
+    //Logout user
+    store.dispatch(logoutUser());
+    //Todo: Clear current Profile
+    //Redirect to Login
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
